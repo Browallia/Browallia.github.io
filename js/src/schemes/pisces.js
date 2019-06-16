0d3bc55da95504a681f6d3711d251d859780961c
Browallia <583415408@qq.com>
2019-06-16 16:39:56 +0800

    Site updated: 2019-06-16 16:39:55

--- js/src/schemes/pisces.js
+++ js/src/schemes/pisces.js
@@ -1,3 +1,3 @@
-// build time:Fri Jun 14 2019 17:14:16 GMT+0800 (GMT+08:00)
+// build time:Sun Jun 16 2019 16:39:46 GMT+0800 (GMT+08:00)
 $(document).ready(function(){var t=$(".sidebar-inner");var i=CONFIG.sidebar.offset?CONFIG.sidebar.offset:12;function e(){return $(".header-inner").height()+i}function r(){var t=$(".footer-inner");var i=t.outerHeight(true)-t.outerHeight();var e=t.outerHeight(true)+i;return e}function n(t){return $("#sidebar").css({"margin-top":t})}function a(){var a=e();var o=r();var f=$("#sidebar").height()+NexT.utils.getSidebarb2tHeight();var c=$("#content").height();if(a+f<c){t.affix({offset:{top:a-i,bottom:o}})}n(a).css({"margin-left":"initial"})}function o(){var i=window.matchMedia("(min-width: 991px)");i.addListener(function(i){if(i.matches){t.affix("checkPosition")}})}a();o();t.affix("checkPosition")});
 //rebuild by neat 
